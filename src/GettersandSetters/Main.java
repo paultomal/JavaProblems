91e574060b6e81601f547fd7649eb89bfafd3f4d
Paul Tamal <94731937+paultomal@users.noreply.github.com>
2023-08-18 13:09:42 +0600

    Getters and Setters

---
+++ src/GettersandSetters/Main.java
@@ -0,0 +1,8 @@
+package GettersandSetters;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Paul", 23, "paul@gmail.com");
+        System.out.println(person);
+    }
+}
---
+++ src/GettersandSetters/Person.java
@@ -0,0 +1,48 @@
+package GettersandSetters;
+
+public class Person {
+    private String name;
+    private Integer age;
+    private String email;
+
+
+    public Person(String name, Integer age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
